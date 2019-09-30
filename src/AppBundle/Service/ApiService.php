b0670e6bd97bca292a0cff14b2d33d02854fc83a
Martin Schröder <martin.ms.schroeder@partner.bmw.de>
2019-09-30 10:03:08 +0200

    Updated RRG version to 11

--- src/AppBundle/Service/ApiService.php
+++ src/AppBundle/Service/ApiService.php
@@ -65,7 +65,7 @@ class ApiService
     public function buildContent ($data = null)
     {
         $content = [];
-        $content['rrg-version'] = '10';
+        $content['rrg-version'] = '11';
         if ($data instanceof SearchInterface) {
             $content['records'] = $data->getRecords();
             $content['size'] = $data->getTotal();
